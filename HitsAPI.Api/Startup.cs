8ac5a9fbb1629f128cd30fe98ade4b78e7c0cd7f
Malek Ammar <Malek.ammar@etudiant-enit.utm.tn>
2021-07-28 12:12:57 +0300

    service layer -crud operation

--- HitsAPI.Api/Startup.cs
+++ HitsAPI.Api/Startup.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HitsAPI.Core;
+using HitsAPI.Core.Services;
 using HitsAPI.Data;
+using HitsAPI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +32,9 @@ namespace HitsAPI.Api
         {
             services.AddControllers();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddDbContext<HitsAPIDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), x => x.MigrationsAssembly("HitsAPI.Data")));
+            services.AddTransient<IMusicService, MusicService>();
+            services.AddTransient<IArtistService, ArtistService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
